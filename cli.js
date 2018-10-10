1fdca9b2e9d1a4568b149fd9ec3ec4f34bf31ce4
RobinzZH <choco-love@qq.com>
2018-10-10 10:17:22 +0800

    feat(cli): support more arguments

--- cli.js
+++ cli.js
@@ -11,16 +11,38 @@ const {
 
 program.version(version);
 
+program.option('-s, --source <source>', 'Path for source code');
+program.option('-p, --path <path>', 'Path for changelog.md(default to root of source code), e.g. xxx.md');
+program.option('-l, --list', 'List the changelog between tags');
+
 program.parse(process.argv);
 
 let source = cwd;
+let logFile = cwd;
 
-if (typeof program.args[0] === 'string') {
-    source = path.resolve(cwd, program.args[0]);
+if (program.source) {
+    source = path.resolve(cwd, program.source);
+}
+
+if (program.path) {
+    logFile = path.resolve(cwd, program.path);
+
+    const extname = path.extname(logFile);
+    if (extname) {
+        if (!extname === '.md') {
+            logFile = path.join(path.resolve(logFile, '../'), './changeLog.md');
+        }
+    } else {
+        logFile = path.join(logFile, './changeLog.md');
+    }
+} else {
+    logFile = path.resolve(source, './changeLog.md');
 }
 
 signale.note(`Generate change @Folder: ${source}`);
 
 main({
-    path: source
+    path: source,
+    LogFile: logFile,
+    genEachLog: program.list
 });
